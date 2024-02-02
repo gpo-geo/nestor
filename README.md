1ee78643c84641062e943eda7097d103b77f0eac
Guillaume Pasero <guillaume.pasero@proton.me>
2024-02-02 22:46:40 +0100

    plug the s3store with tusd

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # nestor
+
 File uploader web service, for training
+
+## Tests with tusd
+
+This package implements the tus protocol to resume uploads. It allows to send large files by chunks,
+and also provides a solution for network interuptions (no need to reupload the file from the start).
+
+Some constrains on client side: the protocol is specific, but compatible with cURL scripting.
+
+* "Creation" step: announce a new upload
+
+```
+curl -i -X POST \
+  -H "Content-Length: 0" \
+  -H "Upload-Length: 49" \
+  -H "Tus-Resumable: 1.0.0" \
+  -H "Upload-Metadata: key value, filename README.md" \
+  http://localhost:8080/files/
+```
+  
+Returns the upload identifier, example: 
+
+```
+HTTP/1.1 201 Created
+Location: http://localhost:8080/files/d1b5a39052fb7a3ea5fb7c5f96c3530a
+Tus-Resumable: 1.0.0
+X-Content-Type-Options: nosniff
+Date: Wed, 31 Jan 2024 22:05:48 GMT
+Content-Length: 0
+```
+
+* "head" step: get current progress
+
+```
+curl -I -X HEAD \
+  -H "Tus-Resumable: 1.0.0" \
+  http://localhost:8080/files/$TOKEN
+```
+
+Returns the current offset.
+
+* "patch" upload a new patch
+
+```
+curl --range 10-19 -o part.dat file:///home/gpasero/coding/nestor/README.md
+curl -i -X PATCH \
+  -H "Content-Type: application/offset+octet-stream" \
+  -H "Content-Length: 10" \
+  -H "Upload-Offset: 0" \
+  -H "Tus-Resumable: 1.0.0" \
+  --data-binary @part.dat \
+  http://localhost:8080/files/$TOKEN
+```
+
+* "download" the complete file
+
+```
+curl -o output.dat http://localhost:8080/files/$TOKEN
+```
+
+
+## Test with encryption
+
+encoding a message, with secret `champignon`:
+
+```
+openssl enc -aes256 -base64 -pass pass:champignon -pbkdf2 -in input.txt -out input.enc
+```
+
+decoding :
+
+```
+openssl enc -d -aes256 -base64 -pass pass:champignon -pbkdf2 -in input.enc -out input.dec
+```
+
+Notes:
+
+* encrypted content is not identic to input message length.
+* the encryption function is not associative: enc(A+B) != enc(A) + enc(B)
+
+* Idea to integrate : make a custom [S3Store](https://pkg.go.dev/github.com/tus/tusd/v2@v2.2.2/pkg/s3store#S3Store) ?
+  or wrapping an existing store ?
--- nestor.go
+++ nestor.go
@@ -1,12 +1,109 @@
 package main
 
 import (
-    "github.com/gpo-geo/nestor/cmd"
+    "context"
+    "fmt"
+    "net/http"
+
+    //~ "github.com/tus/tusd/v2/pkg/filestore"
+    "github.com/tus/tusd/v2/pkg/s3store"
+    "github.com/tus/tusd/v2/pkg/memorylocker"
+    tusd "github.com/tus/tusd/v2/pkg/handler"
+    
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+var Flags struct {
+    S3Bucket                         string
+    S3ObjectPrefix                   string
+    S3Endpoint                       string
+    S3PartSize                       int64
+    S3MaxBufferedParts               int64
+    S3DisableContentHashes           bool
+    S3DisableSSL                     bool
+    S3ConcurrentPartUploads          int
+    S3TransferAcceleration           bool
+}
+
+func setup_default_flags() {
+    Flags.S3Bucket = "gpo"
+    Flags.S3ObjectPrefix = ""
+    Flags.S3Endpoint = "http://localhost:9000"
+    Flags.S3PartSize = 50*1024*1024
+    Flags.S3MaxBufferedParts = 20
+    Flags.S3DisableContentHashes = true
+    Flags.S3DisableSSL = true
+    Flags.S3ConcurrentPartUploads = 10
+    Flags.S3TransferAcceleration = false
+}
 
 func main() {
-    
-    cmd.SetupAndRun()
 
+    setup_default_flags()
+    
+    // Create a S3 storage based on Minio
+    s3Config, err := config.LoadDefaultConfig(context.Background())
+    if err != nil {
+        panic(fmt.Errorf("Unable to load S3 configuration: %s", err))
+    }
+
+    s3Client := s3.NewFromConfig(s3Config, func(o *s3.Options) {
+        o.UseAccelerate = Flags.S3TransferAcceleration
+    
+        // Disable HTTPS and only use HTTP (helpful for debugging requests).
+        o.EndpointOptions.DisableHTTPS = Flags.S3DisableSSL
+    
+        if Flags.S3Endpoint != "" {
+            o.BaseEndpoint = &Flags.S3Endpoint
+            o.UsePathStyle = true
+        }
+    })
+    
+    store := s3store.New(Flags.S3Bucket, s3Client)
+    store.ObjectPrefix = Flags.S3ObjectPrefix
+    store.PreferredPartSize = Flags.S3PartSize
+    store.MaxBufferedParts = Flags.S3MaxBufferedParts
+    store.DisableContentHashes = Flags.S3DisableContentHashes
+    store.SetConcurrentPartUploads(Flags.S3ConcurrentPartUploads)
+    
+    locker := memorylocker.New()
+
+    // A storage backend for tusd may consist of multiple different parts which
+    // handle upload creation, locking, termination and so on. The composer is a
+    // place where all those separated pieces are joined together. In this example
+    // we only use the file store but you may plug in multiple.
+    composer := tusd.NewStoreComposer()
+    store.UseIn(composer)
+    locker.UseIn(composer)
+
+    // Create a new HTTP handler for the tusd server by providing a configuration.
+    // The StoreComposer property must be set to allow the handler to function.
+    handler, err := tusd.NewHandler(tusd.Config{
+        BasePath:              "/files/",
+        StoreComposer:         composer,
+        NotifyCompleteUploads: true,
+    })
+    if err != nil {
+        panic(fmt.Errorf("Unable to create handler: %s", err))
+    }
+
+    // Start another goroutine for receiving events from the handler whenever
+    // an upload is completed. The event will contains details about the upload
+    // itself and the relevant HTTP request.
+    go func() {
+        for {
+            event := <-handler.CompleteUploads
+            fmt.Printf("Upload %s finished\n", event.Upload.ID)
+        }
+    }()
+
+    // Right now, nothing has happened since we need to start the HTTP server on
+    // our own. In the end, tusd will start listening on and accept request at
+    // http://localhost:8080/files
+    http.Handle("/files/", http.StripPrefix("/files/", handler))
+    err = http.ListenAndServe(":8080", nil)
+    if err != nil {
+        panic(fmt.Errorf("Unable to listen: %s", err))
+    }
 }
